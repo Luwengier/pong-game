af7306ce1f2ca5922cfe377fc561da65abcf3841
Luewngier <g217881111@gmail.com>
2023-09-21 21:00:03 +0800

    Turn off tracer and control screen update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
-screen.bgcolor("#021815")
+screen.bgcolor("#021513")
+screen.title("Pong Game")
+screen.tracer(0)
+
+right_paddle = Paddle()
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 
 screen.exitonclick()
