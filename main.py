4d0433179c0d027d53bae055c0fa6ac6b2d5a21c
Luewngier <g217881111@gmail.com>
2023-11-29 23:35:29 +0800

    Bounce ball when colliding with paddles

--- main.py
+++ main.py
@@ -27,7 +27,16 @@ while is_game_on:
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if (
+        ball.distance(right_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(left_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
 
 
 screen.exitonclick()
